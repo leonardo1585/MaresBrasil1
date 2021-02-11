66bfb98a102737e1e2721695dbe6dd728daa9472
Leonardo1585 <lleonardoamaral2011@hotmail.com>
2021-02-11 13:38:12 -0300

    Inicial
    Marés

--- MarApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Lugar
-
-admin.site.register(Lugar)
--- MarApp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Lugar
-
-
-class LugarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lugar
-        fields = ['Lugar', 'Cidade']
